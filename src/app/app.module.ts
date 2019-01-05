378a840e336a4249f4214e323fedd07aaa788b68
Cristian Paniagua <cristianramonpaniagua@gmail.com>
2019-01-05 12:00:34 -0300

    Se agrego el home y el navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { UsuariosComponent } from './usuarios-calito/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
