f37d1c4c7fbff73875b18fc8366cbf5f478464a6
root <root@debian>
2019-01-05 12:34:26 -0300

    se agrego eventos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventosListComponent } from './eventos-list/eventos-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  { path: 'eventos', component: EventosListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { EventosListComponent } from './eventos-list/eventos-list.component';
+import { EventosFormComponent } from './eventos-form/eventos-form.component';
+import { HttpClientModule }    from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosListComponent,
+    EventosFormComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
